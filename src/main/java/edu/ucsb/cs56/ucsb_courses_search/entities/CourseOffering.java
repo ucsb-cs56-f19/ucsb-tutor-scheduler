45e3624a3956a3e4c5dae8e9862711f8fe30e82a
Tanay Komarlu <tkomarlu@ucsb.edu>
2019-11-22 00:45:13 -0800

    Overrided hashCode function for CourseOffering

--- src/main/java/edu/ucsb/cs56/ucsb_courses_search/entities/CourseOffering.java
+++ src/main/java/edu/ucsb/cs56/ucsb_courses_search/entities/CourseOffering.java
@@ -95,4 +95,11 @@ public class CourseOffering {
 
         return true;
     }
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
 }

4a3607017e16f9d1bbd8817713ed2eaf28a4fe69
Tanay Komarlu <tkomarlu@ucsb.edu>
2019-11-23 00:50:50 -0800

    "tk - changed println statements to a logger"

--- src/main/java/edu/ucsb/cs56/ucsb_courses_search/entities/TutorsToCSV.java
+++ src/main/java/edu/ucsb/cs56/ucsb_courses_search/entities/TutorsToCSV.java
@@ -1,11 +1,14 @@
 package edu.ucsb.cs56.ucsb_courses_search.entities;
+import java.io.PrintWriter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.opencsv.CSVWriter;
 
-import java.io.PrintWriter;
 import java.util.List;
 
 public class TutorsToCSV {
+    private static Logger logger = LoggerFactory.getLogger(TutorsToCSV.class);
     public static void writeTutors(PrintWriter writer, List<Tutor> tutors) {
         String[] CSV_HEADER = {"id", "firstname", "lastname", "email"};
         try (
@@ -28,9 +31,9 @@ public class TutorsToCSV {
                 csvWriter.writeNext(data);
             }
             csvWriter.close();
-            System.out.println("Write CSV using CSVWriter successfully!");
+            logger.info("Write CSV using CSVWriter successfully!");
         } catch (Exception e) {
-            System.out.println("Writing CSV error!");
+            logger.info("Writing CSV error!");
             e.printStackTrace();
         }
     }

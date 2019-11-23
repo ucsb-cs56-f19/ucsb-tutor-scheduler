b013c5861e1f3b12901e06a301e908223d5ded75
Tanay Komarlu <tkomarlu@ucsb.edu>
2019-11-23 00:47:19 -0800

    "tk - updated import statements in TutorsToCSV.java"

--- src/main/java/edu/ucsb/cs56/ucsb_courses_search/entities/TutorsToCSV.java
+++ src/main/java/edu/ucsb/cs56/ucsb_courses_search/entities/TutorsToCSV.java
@@ -1,15 +1,9 @@
 package edu.ucsb.cs56.ucsb_courses_search.entities;
-import java.io.PrintWriter;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.opencsv.CSVWriter;
-import com.opencsv.bean.ColumnPositionMappingStrategy;
-import com.opencsv.bean.StatefulBeanToCsv;
-import com.opencsv.bean.StatefulBeanToCsvBuilder;
 
+import java.io.PrintWriter;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class TutorsToCSV {
     public static void writeTutors(PrintWriter writer, List<Tutor> tutors) {

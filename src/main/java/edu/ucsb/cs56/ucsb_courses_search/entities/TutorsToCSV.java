7bd66502a7d5ac88d6ed01edfaf5054a42e330ca
Tanay Komarlu <tkomarlu@ucsb.edu>
2019-11-23 00:38:31 -0800

    "tk - CSV download button now downloads tutor data including id, first name, last name and email"

--- src/main/java/edu/ucsb/cs56/ucsb_courses_search/entities/TutorsToCSV.java
+++ src/main/java/edu/ucsb/cs56/ucsb_courses_search/entities/TutorsToCSV.java
@@ -13,7 +13,31 @@ import java.util.stream.Collectors;
 
 public class TutorsToCSV {
     public static void writeTutors(PrintWriter writer, List<Tutor> tutors) {
-        writer.println("This is a test.");
-        writer.println("If this were a real CSV, it would contain data from the cp object");
+        String[] CSV_HEADER = {"id", "firstname", "lastname", "email"};
+        try (
+                CSVWriter csvWriter = new CSVWriter(writer,
+                        CSVWriter.DEFAULT_SEPARATOR,
+                        CSVWriter.NO_QUOTE_CHARACTER,
+                        CSVWriter.DEFAULT_ESCAPE_CHARACTER,
+                        CSVWriter.DEFAULT_LINE_END);
+        ) {
+            csvWriter.writeNext(CSV_HEADER);
+
+            for (Tutor tutor : tutors) {
+                String[] data = {
+                        tutor.getId().toString(),
+                        tutor.getFname(),
+                        tutor.getLname(),
+                        tutor.getEmail()
+                };
+
+                csvWriter.writeNext(data);
+            }
+
+            System.out.println("Write CSV using CSVWriter successfully!");
+        } catch (Exception e) {
+            System.out.println("Writing CSV error!");
+            e.printStackTrace();
+        }
     }
 }

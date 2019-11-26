e82c3431ddd9ec7299468123e51a0eacff381eb3
Phill Conrad <pconrad@cs.ucsb.edu>
2019-11-25 20:12:20 -0800

    pc/as - fix unit tests by controlling which beans are loaded and fixing syntax errors

--- src/test/java/edu/ucsb/cs56/ucsb_courses_search/TutorsPageTest.java
+++ src/test/java/edu/ucsb/cs56/ucsb_courses_search/TutorsPageTest.java
@@ -5,13 +5,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 // import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder.andExpect;
 
-
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -19,27 +15,38 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.xpath;
+
 import org.junit.Before;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import edu.ucsb.cs56.ucsb_courses_search.controllers.TutorController;
-import edu.ucsb.cs56.ucsb_courses_search.entities.Tutor;
 import edu.ucsb.cs56.ucsb_courses_search.repositories.TutorRepository;
-import edu.ucsb.cs56.ucsb_courses_search.controllers.CourseOfferingController;
-import edu.ucsb.cs56.ucsb_courses_search.repositories.CourseOfferingRepository;
-import edu.ucsb.cs56.ucsb_courses_search.controllers.HomeController;
-import edu.ucsb.cs56.ucsb_courses_search.controllers.RepositoryControllerAdvice;
-import edu.ucsb.cs56.ucsb_courses_search.controllers.RoomsController;
-import edu.ucsb.cs56.ucsb_courses_search.controllers.TimeSlotController;
-import edu.ucsb.cs56.ucsb_courses_search.controllers.TutorAssignmentController;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.authentication;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(TutorController.class)
 public class TutorsPageTest {
+
+    @MockBean
+    private static TutorRepository mockTutorRepository;
+
+    /**
+     * This configures the test to ONLY load one controller,
+     * which means that there are far fewer beans that have to be
+     * mocked and stubbed.   See https://stackoverflow.com/a/45228072
+     * plus the comments underneath about Configuration.
+     */
+    @Configuration
+    public static class TestConf {
+        @Bean
+        public TutorController tutorController() {
+            return new TutorController(mockTutorRepository);
+        }
+    }
 
     @Autowired
     private MockMvc mvc;
@@ -50,52 +57,23 @@ public class TutorsPageTest {
     @MockBean
     private ClientRegistrationRepository crr;
 
-    // @MockBean
-    // private CourseOfferingController coController;
-
-    // @MockBean
-    // private HomeController hc;
-    
-    // @MockBean
-    // private RepositoryControllerAdvice rca;
-
-    // @MockBean
-    // private RoomsController rc;
-
-    // @MockBean
-    // private TimeSlotController tsc;
-
-    // @MockBean
-    // private TutorAssignmentController tac;
-
-    // @MockBean
-    // private TutorController tc;
-
     private OAuth2User principal;
 
     /**
      * Set up an OAuth mock user so that we can unit test protected endpoints
-    */
-     
+     */
+
     @Before
     public void setUpUser() {
         principal = OAuthUtils.createOAuth2User("Chris Gaucho", "cgaucho@example.com");
     }
-   
-
-
-
 
     @Test
     @WithMockUser
     public void createTutorPagehasSubmitButton() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/tutors/create/")
-            .with(authentication(OAuthUtils.getOauthAuthenticationFor(principal)))
-            .accept(MediaType.TEXT_HTML))
-            .andExpect(status().isOk())
-            .andExpect(xpath("/html/body/div/div/div/form/input[2]").exists());
-
-
+                .with(authentication(OAuthUtils.getOauthAuthenticationFor(principal))).accept(MediaType.TEXT_HTML))
+                .andExpect(status().isOk()).andExpect(xpath("/html/body/div/div/div/form/input[2]").exists());
 
     }
 

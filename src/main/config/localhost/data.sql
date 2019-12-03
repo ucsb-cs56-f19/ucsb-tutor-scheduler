d389711ee97d0be957a2188f408608fc4222f8f9
Katelyn Zhang <krzhang@ucsb.edu>
2019-12-02 18:45:04 -0800

    kz - added missing semicolon resulting in errors when running on localhost

--- src/main/config/localhost/data.sql
+++ src/main/config/localhost/data.sql
@@ -4,7 +4,7 @@ INSERT INTO course_offering (course,quarter,instructor) VALUES ('CMPSC 160','F19
 INSERT INTO course_offering (course,quarter,instructor) VALUES ('CMPSC 130A','W20','Koc');
 INSERT INTO course_offering (course,quarter,instructor) VALUES ('CMPSC 130B','w20','Lokshtanov');
 INSERT INTO tutor (fname,lname,email) VALUES ('Scott','Chow','scottpchow@example.org');
-INSERT INTO tutor (fname,lname,email) VALUES ('Zach','Sisco','zachsisco@example.org')
+INSERT INTO tutor (fname,lname,email) VALUES ('Zach','Sisco','zachsisco@example.org');
 INSERT INTO tutor (fname,lname,email) VALUES ('Yinon','Rousso','yinonRousso@example.org');
 INSERT INTO tutor (fname,lname,email) VALUES ('Kate','Perkins','kateperkins@example.org');
 INSERT INTO tutor (fname,lname,email) VALUES ('George','Kripac','Georgekripac@example.org');

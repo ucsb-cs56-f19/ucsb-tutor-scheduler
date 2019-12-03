00e6e1eabb2bdef1c2c256b69f6751752ea4bb5b
Tanay Komarlu <tkomarlu@ucsb.edu>
2019-12-02 16:30:07 -0800

    reverted changes to data.sql file

--- src/main/config/localhost/data.sql
+++ src/main/config/localhost/data.sql
@@ -1,5 +1,1 @@
-INSERT INTO course_offering (course,quarter,instructor) VALUES ('CMPSC 56','F19','Conrad');
-INSERT INTO course_offering (course,quarter,instructor) VALUES ('CMPSC 8','F19','Mirza');
-INSERT INTO course_offering (course,quarter,instructor) VALUES ('CMPSC 64','F19','Matni');
-INSERT INTO course_offering (course,quarter,instructor) VALUES ('CMPSC 48','F18','Matni');
 INSERT INTO tutor (fname,lname,email) VALUES ('Scott','Chow','scottpchow@example.org');

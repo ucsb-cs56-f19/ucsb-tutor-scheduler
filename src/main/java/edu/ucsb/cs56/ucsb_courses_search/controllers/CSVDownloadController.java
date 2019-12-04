7243f91278cd89b2d35fc6d5cdc6f04a1dd9d54f
Tanay Komarlu <tkomarlu@ucsb.edu>
2019-12-03 18:40:17 -0800

    optimized import statements and reverted changes to data.sql

--- src/main/java/edu/ucsb/cs56/ucsb_courses_search/controllers/CSVDownloadController.java
+++ src/main/java/edu/ucsb/cs56/ucsb_courses_search/controllers/CSVDownloadController.java
@@ -4,17 +4,12 @@ import edu.ucsb.cs56.ucsb_courses_search.entities.Tutor;
 import edu.ucsb.cs56.ucsb_courses_search.entities.TutorsToCSV;
 import edu.ucsb.cs56.ucsb_courses_search.repositories.TutorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.io.IOException;
-import java.util.List;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
-
-
-import org.springframework.web.bind.annotation.RestController;
+import java.io.IOException;
+import java.util.List;
 
 @RestController
 public class CSVDownloadController {

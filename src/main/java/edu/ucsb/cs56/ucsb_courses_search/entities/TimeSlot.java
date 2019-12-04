238becd653a53f657c26b6ead77d8fd9f85098fb
Katelyn Zhang <43655384+katelynzzz@users.noreply.github.com>
2019-12-04 14:21:22 -0800

    kz - changed id generation to IDENTITY to fix data.sql

--- src/main/java/edu/ucsb/cs56/ucsb_courses_search/entities/TimeSlot.java
+++ src/main/java/edu/ucsb/cs56/ucsb_courses_search/entities/TimeSlot.java
@@ -11,7 +11,7 @@ import javax.validation.constraints.NotBlank;
 public class TimeSlot {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     @NotBlank(message = "Start time is mandatory (e.g. 06:00PM)")

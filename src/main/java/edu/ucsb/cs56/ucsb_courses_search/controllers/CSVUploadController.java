6e31f5a3c55c77e23452f70e2afffea5caff3b1a
Tanay Komarlu <tkomarlu@ucsb.edu>
2019-12-03 17:08:41 -0800

    tk - removed unused imports and comments

--- src/main/java/edu/ucsb/cs56/ucsb_courses_search/controllers/CSVUploadController.java
+++ src/main/java/edu/ucsb/cs56/ucsb_courses_search/controllers/CSVUploadController.java
@@ -2,30 +2,22 @@ package edu.ucsb.cs56.ucsb_courses_search.controllers;
 
 import edu.ucsb.cs56.ucsb_courses_search.entities.CSVToTutors;
 import edu.ucsb.cs56.ucsb_courses_search.entities.Tutor;
-import edu.ucsb.cs56.ucsb_courses_search.entities.TutorBean;
 import edu.ucsb.cs56.ucsb_courses_search.repositories.TutorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.io.IOException;
-import java.nio.file.Paths;
 import java.util.List;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class CSVUploadController {
     @Autowired
     TutorRepository tutorRepository;
-
-    private Logger logger = LoggerFactory.getLogger(TutorController.class);
 
     @RequestMapping(path = "/tutorCSVUpload", method = RequestMethod.POST)
     public String uploadCSV(@RequestParam("file") MultipartFile file,
--- src/main/java/edu/ucsb/cs56/ucsb_courses_search/entities/CSVToTutors.java
+++ src/main/java/edu/ucsb/cs56/ucsb_courses_search/entities/CSVToTutors.java
@@ -1,27 +1,19 @@
 package edu.ucsb.cs56.ucsb_courses_search.entities;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.net.URI;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.opencsv.bean.ColumnPositionMappingStrategy;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.util.StringUtils;
 
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Iterator;
+import java.util.List;
 
 public class CSVToTutors {
-    private static Logger logger = LoggerFactory.getLogger(CSVToTutors.class);
 
     public static List<Tutor> tutorBuilder(MultipartFile file) throws Exception {
         try {
@@ -44,8 +36,6 @@ public class CSVToTutors {
                 TutorBean tutorBean = csvTutorIterator.next();
                 Tutor tutor = new Tutor(tutorBean);
                 tutors.add(tutor);
-                //logger.info(tutor.toString());
-                //logger.info(tutorBean.getId() + " " + tutorBean.getFname() + " " + tutorBean.getLname() + " " + tutorBean.getEmail());
             }
             return tutors;
         } catch (IOException e) {
